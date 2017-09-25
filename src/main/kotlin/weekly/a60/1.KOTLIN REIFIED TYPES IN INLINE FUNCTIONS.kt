bd1027d9be02ca7053514bbfe567ead3b772c049
kimtaesu <kimtaesoo188@gmail.com>
2017-09-25 22:52:36 +0900

    initfile

--- src/main/kotlin/weekly/a60/1.KOTLIN REIFIED TYPES IN INLINE FUNCTIONS.kt
+++ src/main/kotlin/weekly/a60/1.KOTLIN REIFIED TYPES IN INLINE FUNCTIONS.kt
@@ -1,5 +1,24 @@
 package weekly.a60
 
-fun main(args: Array<String>) {
 
+
+//fun <T> getTNameOriginal(t: T): String {
+//    Compile Error
+//    return T::class.java.name
+//}
+
+
+fun <T> getTNameWithOutReified(t: Class<T>): String {
+    return t.name
+}
+
+inline fun <reified T> getTName(t: T): String {
+    return T::class.java.name
+}
+
+
+
+
+fun main(args: Array<String>) {
+println(getTName(String))
 }
