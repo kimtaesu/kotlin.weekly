25bf624d38f2c9399fa7777b771b66d686fa1294
kimtaesu <kimtaesoo188@gmail.com>
2017-09-30 20:00:22 +0900

    3. 2_Spek_your_reactive_stream_using_PublishSubjectsTest.kt

--- src/test/kotlin/weekly/a60/2_Spek_your_reactive_stream_using_PublishSubjectsTest.kt
+++ src/test/kotlin/weekly/a60/2_Spek_your_reactive_stream_using_PublishSubjectsTest.kt
@@ -3,9 +3,12 @@ package weekly.a60
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.reset
 import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Observable
+import io.reactivex.functions.BiFunction
 import io.reactivex.observers.TestObserver
 import io.reactivex.subjects.PublishSubject
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
@@ -20,7 +23,9 @@ class SpekPublishSubjectTest : Spek({
     var unitSub: PublishSubject<Int> = PublishSubject.create()
 
     var testSubscriber = TestObserver<Int>()
+
     given("a GPS system") {
+
         beforeEachTest {
             distanceSub = PublishSubject.create()
             unitSub = PublishSubject.create()
@@ -29,13 +34,48 @@ class SpekPublishSubjectTest : Spek({
 
             testSubscriber = tested.distances().test()
         }
-
         on("a distance update of 30 km") {
             distanceSub.onNext(listOf(30, 19)) // [30km, 19miles]
             unitSub.onNext(0) // km
 
             it("should emit the value 30") {
                 testSubscriber.assertValue(30)
+            }
+            it("should not complete") {
+                testSubscriber.assertNotComplete()
+            }
+            it("should not throw errors") {
+                testSubscriber.assertNoErrors()
+            }
+        }
+        context("distance update of 30 km") {
+            beforeEachTest {
+                distanceSub.onNext(listOf(30, 19))
+                unitSub.onNext(0)
+            }
+
+            it("should emit the value 30") {
+                testSubscriber.assertValue(30)
+            }
+
+            context("unit update to miles") {
+                beforeEachTest {
+                    unitSub.onNext(1)
+                }
+
+                it("should emit 19 as second value") {
+                    testSubscriber.assertValues(30, 19)
+                }
+
+                context("no more units") {
+                    beforeEachTest {
+                        unitSub.onComplete()
+                    }
+
+                    it("should not complete") {
+                        testSubscriber.assertNotComplete()
+                    }
+                }
             }
         }
         afterEachTest {
@@ -44,3 +84,28 @@ class SpekPublishSubjectTest : Spek({
         }
     }
 })
+
+class DistancesProvider {
+    fun distances(): Observable<List<Int>> {
+        return Observable.just(listOf())
+    }
+
+}
+
+class UnitProvider {
+
+    fun unit(): Observable<Int> {
+        return Observable.just(0)
+    }
+}
+
+class DistancesUseCase(val distancesProvider: DistancesProvider,
+                       val unitProvider: UnitProvider) {
+    fun distances(): Observable<Int> {
+        return Observable.combineLatest(
+                distancesProvider.distances(),
+                unitProvider.unit(),
+                BiFunction({ distances, unit -> distances[unit] })
+        )
+    }
+}
